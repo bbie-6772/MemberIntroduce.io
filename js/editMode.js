c887032ba924ac0752de1e4f40e18fb3c25b5d08
lgm-7 <xlzlrud@naver.com>
2024-10-31 16:36:05 +0900

    edit
    비효율적 관리자모드

--- js/editMode.js
+++ js/editMode.js
@@ -22,7 +22,7 @@ $('#editbtn').click(async function () {
 
             if (data.key === key) {
                 alert('관리자로 확인이 되었습니다.');
-                window.location.href = 'index.html';
+                window.location.href = 'index2.html';
 
                 // 세션에 로그인 상태 저장
                 sessionStorage.setItem('editMode', 'true');
@@ -35,5 +35,6 @@ $('#editbtn').click(async function () {
     } else {
         sessionStorage.clear();
         window.location.reload();
+        window.location.href = 'index.html';
     }
 });
